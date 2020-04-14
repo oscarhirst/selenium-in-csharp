da81010ae1271019e1577c5fc8ac21847c1f7258
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2020-04-14 10:54:31 +0100

    copied in files and started writing first test

---
+++ CreditCards/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,18 @@
+﻿// This work is based on the Pluralsight course 'Creating Automated Browser Tests with Selenium in C#' by Jason Roberts
+
+namespace CreditCards.UITests
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    [TestFixture]
+    public class CreditCardWebAppShould
+    {
+        [Test]
+        public void LoadApplicationPage()
+        {
+            using IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
