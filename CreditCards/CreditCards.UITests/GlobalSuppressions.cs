aad13eb5f07c1b5930c8eafce7972502202865ca
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2020-04-14 10:58:53 +0100

    codestyle corrections

--- CreditCards/CreditCards.UITests/GlobalSuppressions.cs
+++ CreditCards/CreditCards.UITests/GlobalSuppressions.cs
@@ -1,8 +1,7 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// <copyright file="GlobalSuppressions.cs" company="Corsham Science">
+// Copyright (c) Corsham Science. All rights reserved.
+// </copyright>
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>", Scope = "namespace", Target = "~N:CreditCards.UITests")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "not relevant")]
